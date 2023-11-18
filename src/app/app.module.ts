875bb64ec1bad630f96a4a9f5916418511a0e6a5
Joshua McCarthy <joshua.f.mccarthy@gmail.com>
2023-11-18 23:35:09 +0900

    Created new Angular app via cli tool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
